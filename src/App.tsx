b37df0c12b3729f8e86f51ac737cd7e7ebb7bbd0
NeoMemb <arioriabdulrafiu220@gmail.com>
2025-10-27 07:06:26 +0100

    Added font-awesome import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import all the icons in Free Solid, Free Regular, and Brands styles */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import { links, details, photos, personalInfo } from './data/portfolio.ts'
 import './App.css'
+
+library.add(fas, far, fab)
 
 const Links = ({ value, hrefs, className }: { value: string, hrefs: string, className?: string }) => (
   <a href={hrefs} className={className}>{value}</a>
@@ -26,15 +35,15 @@ const App = ()=> {
       </header>
       
       {/* Main */}
-      <main className="home">
+      <main className="main">
         <section className="home" id="home">
         <div className="home-img">
           <img className="dp-img" src={photos.dark.dp} alt={photos.dark.alt} />
         </div>
         <div className="home-content">
           <h1>Hi, It's <span className="blue-txt highlight">Ariori</span>...</h1>
-          <h3 className="typing-text">I'm a <span >{personalInfo.role}</span></h3>
-          <p className="landing-info letter-space">{personalInfo.description}</p>
+          <h3 className="typing-text">I'm a <span className="typing-text">{personalInfo.role}</span></h3>
+          <p className="landing-info letter-space no-select">{personalInfo.description}</p>
           <div className="social-icons">
               <a href="#"><i className="fa-brands fa-linkedin"></i></a>
               <a href="#"><i className="fa-brands fa-github"></i></a>
