b6550f4e15579d6ec8dc63bd4e4b7192fb38d86b
NeoMemb <arioriabdulrafiu220@gmail.com>
2025-12-12 17:00:59 +0100

    Just removed unused components, Link.tsx & Button.tsx

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { type ButtonProp } from "../types/portfolio.ts"
-
-const Button = ({ children, className, onClick, id } : ButtonProp) => (
-    <button className={className} id={id} onClick={onClick}>
-        {children}
-    </button>
-)
-
-export { Button }
--- src/components/Link.tsx
+++
@@ -1,7 +0,0 @@
-import { type AnchorLink } from "../types/portfolio";
-
-const Link = ({ value, hrefs, className }: AnchorLink) => (
-  <a href={hrefs} className={className}>{value}</a>
-)
-
-export { Link }
